0d308c8686b2153929f8b1c829a973b8500a116c
chlwoqls <byin0209@naver.com>
2024-04-01 16:48:02 +0900

    로그인 body -> params 변경

--- src/main/java/com/animal/user/api/controller/UserController.java
+++ src/main/java/com/animal/user/api/controller/UserController.java
@@ -1,32 +1,29 @@
 package com.animal.user.api.controller;
 
 import com.animal.user.api.dto.UserDTO;
-import com.animal.user.api.model.User;
+
 import com.animal.user.api.response.Response;
 import com.animal.user.api.service.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+
 
 
 @RestController
 @RequestMapping("user")
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     @Autowired
     public UserController(UserService userService){
@@ -43,9 +40,9 @@ public class UserController {
 
 
     @GetMapping(value = "/sign-in")
-    public Response<?> signIn(@RequestBody UserDTO dto){
+    public Response<?> signIn(@RequestParam("email") String email, @RequestParam("password")String password){
 
-        return userService.login(dto.getEmail(),dto.getPassword());
+        return userService.login(email,password);
 
     }
 
